1f36036846aa34ba9cd6676f01439dc6eb72850f
Buddiz.io <johann@buddiz.io>
2018-08-29 17:27:42 +0200

    first push

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('running !')
+var express = require('express');
+var app = express();
+
+let port = 8081;
+console.log('app listening on port ' + port);
+app.get('/',function(req,res) {
+  res.send("Hello World");
+});
+
+app.listen(port);
